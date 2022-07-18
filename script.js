3bc012f7d5466e628bc21a2948047baec6136092
Tomas Pocius <pocius.tomastp@gmail.com>
2022-07-18 09:27:39 +0300

    all functions seems to work

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const ticTacToe = (() => {
     let gameEnebled = true;
-    window.onload = function () {
-        if(players.computer.active === "yes" && players.computer.start === "yes") {
+    window.onload = function () {        
+        if(players.computer.active === true && players.computer.start === true) {
            computer.modes();
+        }; 
+        let player1 = document.getElementById("player1choice"); 
+        let player2 = document.getElementById("player2choice"); 
+        if (players.player1 === "o") {
+        player1.innerHTML = "\u2D54";
+        player1.style.color = "red"; 
+        player2.innerHTML = "\u2718";
+        player2.style.color = "black"
+        } else if (players.player1 === "x"){
+            player2.innerHTML = "\u2D54";
+            player2.style.color = "red"; 
+            player1.innerHTML = "\u2718"; 
+            player1.style.color = "black"
+        };
         }
-    }
+        
     const Gameboard = {
         gameboard: Array.from(document.getElementsByClassName("cell")),
         remainingCellId: (() => { 
@@ -19,11 +33,13 @@ const ticTacToe = (() => {
         cellsClickedId: [],
         lastClickedCellNr: []
     };
-    const players = new function () {        
+    const players = new function () {
+        this.player1Score = 0;
+        this.player2Score = 0; 
         this.start = "player1";
         this.computer = {
-            active: "yes",
-            start: "yes",
+            active: false,
+            start: false,
             mode: "easy"
         };
         this.player1 = "o";
@@ -48,9 +64,12 @@ const ticTacToe = (() => {
         }) 
     }
     const computer = {
-            modes: function() {
-                
-                if (players.computer.active === "yes") {                
+            modes: function() { 
+                let leftChoices = Gameboard.remainingCellId.allCells.length;                    
+                let randomNr = Math.floor(Math.random()*leftChoices);
+                let randomChoice = Gameboard.remainingCellId.allCells[randomNr];
+                let randomChoiceArr = Gameboard.gameboard.find(x => x.id === randomChoice)               
+                if (players.computer.active === true) {                
                     let remainingCells = (() => { 
                     let lastClickedId  = Gameboard.cellsClickedId[Gameboard.cellsClickedId.length - 1]                
                     for (let j = 0; Gameboard.remainingCellId.allCells.length > j; j++) {
@@ -58,23 +77,21 @@ const ticTacToe = (() => {
                                 Gameboard.remainingCellId.allCells.splice(j, 1);                            
                             }
                 })();
-                easy = (() => {
-                    if (players.computer.mode === "easy" && players.computer.start === "yes") {
-                        let leftChoices = Gameboard.remainingCellId.allCells.length;                    
-                        let randomNr = Math.floor(Math.random()*leftChoices);
-                        let randomChoice = Gameboard.remainingCellId.allCells[randomNr];
-                        let randomChoiceArr = Gameboard.gameboard.find(x => x.id === randomChoice)
-                        if(Gameboard.XandOhistory.length === 0 || Gameboard.XandOhistory.length % 2 === 0 && gameEnebled === true) {
-                            gameFlow.XandO(randomChoiceArr);
-                            
+                easy = (() => {                    
+                    if (players.computer.mode === "easy" && players.computer.start === true && gameEnebled === true) {                        
+                        if(Gameboard.XandOhistory.length === 0 || Gameboard.XandOhistory.length % 2 === 0 ) {
+                            gameFlow.XandO(randomChoiceArr);                            
                         }
-
+                    } else if (players.computer.mode === "easy" && players.computer.start === false && gameEnebled === true) {
+                        if(Gameboard.XandOhistory.length % 2 === 1) {
+                            gameFlow.XandO(randomChoiceArr);                            
+                        }
                     }
                     })()
                 }
             }
         }
-
+    
 
 
     const gameFlow = {
@@ -105,9 +122,9 @@ const ticTacToe = (() => {
             Gameboard.cellsClickedId.push(element.getAttribute("id"));
             lastClickedCellNrSquare();
             victory();  
-            computer.modes();
-                    
+            computer.modes(); 
             
+
         }
         
     };
@@ -134,7 +151,16 @@ const ticTacToe = (() => {
         [0, 4, 8],
         [2, 4, 6]
     ];
-    
+    const popUp = {
+       box:  function () {
+        document.getElementById("pop-up").style.display = "block"
+       },            
+       boxtext: function(player, text) {
+        document.getElementById("pop-up-text1").textContent = `${player}`;
+        document.getElementById("pop-up-text2").textContent = `${text}`;
+       }
+      
+    }
         
     const victory = () => {
         let win = (() => {
@@ -144,23 +170,52 @@ const ticTacToe = (() => {
                         WinningCombinations[i].splice(j, 1);
                         WinningCombinations[i].push(Gameboard.XandOhistory[Gameboard.XandOhistory.length -1])
                     }              
-                    if (WinningCombinations[i].toString() === `o,o,o` || WinningCombinations[i].toString() === `x,x,x`) {
-                        console.log ("win");   
-                        gameEnebled = false;   
-                        break;                                  
-                    }  
-                };  
-              
-                        
+                    if (WinningCombinations[i].toString() === `o,o,o`) {                        
+                        if(players.computer.active === true && players.player1 === "x"){
+                            popUp.boxtext("Computer", "Wins!!!"); 
+                            buttonFunctions.score("player2")                       
+                        } else if (players.computer.active === true && players.player1 === "o") {
+                            popUp.boxtext("You", "Win!!!"); 
+                            buttonFunctions.score("player1")   
+                        } else if (players.computer.active === false && players.player1 === "x") {
+                            popUp.boxtext("Player2", "Wins!!!"); 
+                            buttonFunctions.score("player2")  
+                        }else if (players.computer.active === false && players.player1 === "o") {
+                            popUp.boxtext("Player1", "Wins!!!");
+                            buttonFunctions.score("player1")  
+                        };
+                        popUp.box();   
+                        gameEnebled = false;                       
+                        break;                                                          
+                    } else if (WinningCombinations[i].toString() === `x,x,x`) {
+                        if(players.computer.active === true && players.player1 === "o"){
+                            popUp.boxtext("Computer", "Wins!!!"); 
+                            buttonFunctions.score("player2")                       
+                        } else if (players.computer.active === true && players.player1 === "x") {
+                            popUp.boxtext("You", "Win!!!"); 
+                            buttonFunctions.score("player1")  
+                        } else if (players.computer.active === false && players.player1 === "o") {
+                            popUp.boxtext("Player2", "Wins!!!");
+                            buttonFunctions.score("player2")   
+                        }else if (players.computer.active === false && players.player1 === "x") {
+                            popUp.boxtext("Player1", "Wins!!!");
+                            buttonFunctions.score("player1")  
+                        };
+                        popUp.box();   
+                        gameEnebled = false;                      
+                        break;                           
+                    }
+                };                         
             }
             if (Gameboard.XandOhistory.length === 9 && gameEnebled === true)  {  
-                    console.log("draw");
-                    gameEnebled = false;
+                popUp.boxtext("It`s a", "Draw!!!");
+                popUp.box();
+                gameEnebled = false;
             } 
         })();
     }; 
-    const buttons = {
-        reset: document.getElementById("reset").addEventListener("click", () => {
+     
+    const reset = function () {
             gameEnebled = true;
             WinningCombinations = [
                     [0, 1, 2],
@@ -187,11 +242,84 @@ const ticTacToe = (() => {
                         }
                         return {allCells}
                     })();
-            window.onload ()
+            window.onload ()          
+        }
+    const buttonFunctions = {
+            switchChoices: function () {
+            if (players.player1 === "x"){
+                players.player1 = "o";
+                reset();
+            } else if (players.player1 === "o"){
+                players.player1 = "x";
+                reset();
+            }
+            },
+            pvp: function () {
+                players.computer.active = false;
+                document.getElementById("player2-computer").innerHTML = "Player 2";
+                reset();
+            },
+            cvp: function () {
+                players.computer.active = true;
+                document.getElementById("player2-computer").innerHTML = "&nbsp&nbsp&nbsp&nbsp&nbspEasy";
+                reset();
+            },
+            score: function (player) {
+                if(player === "player1"){
+                    players.player1Score += 1;
+                    document.getElementById("player1score").innerText = players.player1Score;                  
+                } else if (player === "player2"){
+                    players.player2Score += 1;
+                    document.getElementById("player2score").innerText = players.player2Score;
+                }
 
-           
-        })
-    }  
+            },
+            popUpClose: function () {
+                document.getElementById("pop-up").style.display = "none";                
+                if (players.start === "player1"){
+                    if(players.computer.active === true){
+                    players.computer.start = true;
+                }
+                    players.start = "player2"
+                } else if (players.start === "player2"){
+                    if(players.computer.active === true){
+                        players.computer.start = false;
+                    }
+                    players.start = "player1"
+                }
+                reset();
+            },
+            resetScore: function () {
+                players.player1Score = 0;
+                players.player2Score = 0; 
+                document.getElementById("player1score").innerHTML = 0;
+                document.getElementById("player2score").innerHTML = 0;
+            }
+            
+    }
+    
+const buttons = {
+        reset:
+            document.getElementById("reset").addEventListener("click", () => {
+                reset();
+                buttonFunctions.resetScore();
+            }),
+        switchChoices : 
+            document.getElementById("switch-choices").addEventListener("click", buttonFunctions.switchChoices),
+        pvp:
+            document.getElementById("pvp").addEventListener("click",() => {
+                 buttonFunctions.pvp();
+                 buttonFunctions.resetScore();
+            }),
+        cvp:
+            document.getElementById("cvp").addEventListener("click", () => {
+                buttonFunctions.cvp();
+                buttonFunctions.resetScore();
+            }),
+        popUpClose: 
+            document.getElementById("pop-up").addEventListener("click", buttonFunctions.popUpClose)
+}
+     
     return {Gameboard, players}
 })();
         
